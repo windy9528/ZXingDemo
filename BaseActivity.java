4b2a64af31caa87895d105cfbaf21d096a896e6e
windy <a952814576@163.com>
2019-06-06 15:10:22 +0800

    Initial commit

---
+++ BaseActivity.java
@@ -0,0 +1,23 @@
+package bwie.com.zxingdemo;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.uuzuche.lib_zxing.activity.CodeUtils;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
+    }
+
+}
